a8aa8724a694b17889aeb76620ac2f68f672f037
Gabriel Silva <gabriel.silva.ext@einstein.br>
2024-05-30 21:37:01 -0300

    add: swc and fix types

---
+++ src/third-party/dataloader/types/adjacency-graph.ts
@@ -0,0 +1,37 @@
+export class AdjacencyGraph<V, E> {
+  private readonly adjacencyList: Map<V, Map<V, E>>;
+
+  constructor() {
+    this.adjacencyList = new Map();
+  }
+
+  addVertex(vertex: V) {
+    if (!this.adjacencyList.has(vertex)) {
+      this.adjacencyList.set(vertex, new Map());
+    }
+  }
+
+  addEdge(vertex1: V, vertex2: V, edge: E) {
+    if (!this.adjacencyList.has(vertex1)) {
+      this.addVertex(vertex1);
+    }
+
+    if (!this.adjacencyList.has(vertex2)) {
+      this.addVertex(vertex2);
+    }
+    this.adjacencyList.get(vertex1)?.set(vertex2, edge);
+  }
+
+  transform<NewVertices, NewEdges>(resolveVertexFn: (vertex: V) => NewVertices, resolveEdgesFn: (edge: E) => NewEdges) {
+    const newGraph = new AdjacencyGraph<NewVertices, NewEdges>();
+    for (const [vertex, edges] of this.adjacencyList) {
+      const resolvedVertex = resolveVertexFn(vertex);
+      newGraph.addVertex(resolvedVertex);
+      for (const [neighbor, value] of edges) {
+        const resolvedNeighbor = resolveVertexFn(neighbor);
+        newGraph.addEdge(resolvedVertex, resolvedNeighbor, resolveEdgesFn(value));
+      }
+    }
+    return newGraph;
+  }
+}
